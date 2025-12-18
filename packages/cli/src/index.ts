f633e90d0669aeedc243d65b85c293f86f2a96df
utnim2 <mintugogoi567@gmail.com>
2025-12-18 16:30:00 +0530

    feat: initialize CLI package with oclif

---
+++ packages/cli/src/index.ts
@@ -0,0 +1,9 @@
+/**
+ * ZCraft CLI - Interactive CLI for FHEVM development
+ *
+ * Main entry point for the @oclif CLI framework
+ *
+ * @packageDocumentation
+ */
+
+export { run } from '@oclif/core';
